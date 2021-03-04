c3ca5a8c36b63b72a651cca307020ff5d9afe250
wilvanos zhira <lex540@live.com>
2021-03-04 21:03:38 +0200

    crash fix

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.use('/api',goals);
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+app.listen(PORT, ()=>{
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+})
+//app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
