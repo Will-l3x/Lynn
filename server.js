b0796847d8af07c6f35ffcae06574d6fdbc9f175
wilvanos zhira <lex540@live.com>
2021-03-11 12:09:48 +0200

    kik

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/api', transactions);
 app.use('/api',goals);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, ()=>{
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
